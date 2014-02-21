eee7dc9174bb5977ad8c7ac816af2f180f2f351e
Harry Ng <harryworld@gmail.com>
2014-02-21 18:46:39 +0800

    Pass the test of is_mongoid_model?

--- lib/my_mongoid/document.rb
+++ lib/my_mongoid/document.rb
@@ -1,7 +1,15 @@
 module MyMongoid
   module Document
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
 
     module ClassMethods
+
+      # Check if it is a MyMongoid model
+      def is_mongoid_model?
+        true
+      end
     end
   end
 end
